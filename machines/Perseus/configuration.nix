b20ba91b19266c9d94560731e0706e9fd3802bf0
Cyrus Fung <cyhfung@gmail.com>
2026-01-09 02:02:51 -0500

    fix: WSL clipboard

--- machines/Perseus/configuration.nix
+++ machines/Perseus/configuration.nix
@@ -19,5 +19,19 @@
     grub.enable = false;
     systemd-boot.enable = false;
   };
-}
 
+  environment.sessionVariables = {
+    XDG_RUNTIME_DIR = "/mnt/wslg/runtime-dir";
+    WAYLAND_DISPLAY = "wayland-0";
+    DISPLAY = ":0"; # For X11 fallback
+  };
+
+  system.activationScripts.fix-wslg-socket = {
+    text = ''
+      # Link the WSLg socket to the standard Linux location as a backup
+      mkdir -p /run/user/1000
+      ln -sf /mnt/wslg/runtime-dir/wayland-0 /run/user/1000/wayland-0
+      ln -sf /mnt/wslg/runtime-dir/wayland-0.lock /run/user/1000/wayland-0.lock
+    '';
+  };
+}
