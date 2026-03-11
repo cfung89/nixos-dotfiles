ea454f39003d779a6792351d62a67c6c2b408ff2
Cyrus Fung <cyhfung@gmail.com>
2026-03-10 23:44:09 -0400

    fix treesitter && add undotree

--- config/nvim/lua/config/plugins/treesitter.lua
+++ config/nvim/lua/config/plugins/treesitter.lua
@@ -4,20 +4,14 @@ return {
 		build = ":TSUpdate",
 		event = { "BufReadPost", "BufNewFile" },
 		config = function()
-			vim.api.nvim_create_autocmd('FileType', {
-				pattern = { '<filetype>' },
-				callback = function() vim.treesitter.start() end,
-			})
-			require("nvim-treesitter").setup({
-				ensure_installed = {},
-				sync_install = false,
-				auto_install = true,
-				ignore_install = {},
-				indent = { enable = true },
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = { "markdown" },
-				},
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "*",
+				callback = function(args)
+					local lang = vim.bo[args.buf].filetype
+					if lang ~= "" and pcall(vim.treesitter.get_parser, args.buf, lang) then
+						vim.treesitter.start(args.buf)
+					end
+				end,
 			})
 		end
 	}
